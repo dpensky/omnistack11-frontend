1dd8d5ab69b6761ae0e812bd5a96e81b8f0c0492
Darcio <dpensky@gmail.com>
2020-03-25 18:17:43 -0300

    tela de cadastro ok

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import { FiLogIn } from 'react-icons/fi';
+
+import './styles.css';
+// import logoImg from '../../assets/logo.svg';
+// import heroesImg from '../../assets/heroes.png';
+
+export default function Profile() {
+    return (
+        <h1>Profile</h1>
+    );
+}
